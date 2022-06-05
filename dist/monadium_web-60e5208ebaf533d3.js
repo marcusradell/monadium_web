9abebbd4ebf88cfb46828fbfb419e9d3bea45c9f
Marcus Rådell <marcus@radell.net>
2022-06-05 20:30:24 +0200

    remove unused import

--- dist/monadium_web-60e5208ebaf533d3.js
+++ dist/monadium_web-60e5208ebaf533d3.js
@@ -270,7 +270,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('monadium_web-6394083a053e6f7a_bg.wasm', import.meta.url);
+        input = new URL('monadium_web-60e5208ebaf533d3_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
--- src/domains/profiles/profile.rs
+++ src/domains/profiles/profile.rs
@@ -1,5 +1,3 @@
-use std::error::Error;
-
 use super::Gig;
 use chrono::{Date, Datelike, TimeZone, Utc};
 
